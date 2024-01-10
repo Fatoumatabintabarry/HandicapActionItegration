441f5b2a32d8cea27cf544b2c8cfd4e787786ad8
Fatoumatabintabarry <97992954+Fatoumatabintabarry@users.noreply.github.com>
2024-01-09 23:01:54 -0500

    Navbar component
    1rst iteration of the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image'
+import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
-    <h1 className='font-bold'> 
-    Hello world
-     </h1>
+    <div></div>
   )
 }
